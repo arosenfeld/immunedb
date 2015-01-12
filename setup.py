c22a785385db1b9b88a935bd525d5c134e4caa61
Aaron Rosenfeld <ar374@drexel.edu>
2015-01-12 11:18:59 -0500

    Release version 4.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='SLDB',
-      version='4.0.0',
+      version='4.0.3',
       author='Aaron M. Rosenfeld',
       author_email='ar374@drexel.edu',
       packages=[
