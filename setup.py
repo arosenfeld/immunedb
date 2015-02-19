a0efb1f50a7780a882431f5ddec6a879cfa6fd20
Aaron Rosenfeld <ar374@drexel.edu>
2015-02-19 16:19:43 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='SLDB',
-      version='6.1.1',
+      version='6.1.2',
       author='Aaron M. Rosenfeld',
       author_email='ar374@drexel.edu',
       packages=[
