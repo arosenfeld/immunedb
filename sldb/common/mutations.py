c023fde83e0b2841b0c0d4a4d20eeba3a015511a
Aaron Rosenfeld <ar374@drexel.edu>
2015-03-13 11:34:49 -0400

    mutation counting

--- sldb/common/mutations.py
+++ sldb/common/mutations.py
@@ -4,16 +4,47 @@ from sldb.common.models import Sequence
 from sldb.identification.v_genes import VGene
 import sldb.util.lookups as lookups
 
-class MutationCount(object):
-    def __init__(self, session, clone, seqs=None):
+class ContextualMutations(object):
+    def __init__(self):
+        self._seen = set([])
+        self._mutations = {
+            'total': {},
+            'unique': {},
+        }
+
+    def add_mutation(self, seq_replaced, mtype, mutation, copy_number):
+        pos, from_nt, to_nt, from_aa, to_aa = mutation
+        uniq = (pos, from_nt, to_nt)
+        # If it's a new mutation, setup the dictionaries
+        if uniq not in self['total']:
+            self._mutations['total'][uniq] = {}
+            self._mutations['unique'][uniq] = {}
+
+        # If the mutation type has not been seen for the mutation, setup the
+        # dictionaries
+        if mtype not in record['total'][uniq]:
+            record['total'][uniq][mtype] = {}
+            record['unique'][uniq][mtype] = {}
+
+        # If the specific amino acid change has not been seen, setup the
+        # counters
+        if (from_aa, to_aa) not in record['total'][uniq][mtype]:
+            record['total'][uniq][mtype][(from_aa, to_aa)] = 0
+            record['unique'][uniq][mtype][(from_aa, to_aa)] = 0
+
+        # Increment the total count for the mutation
+        record['total'][uniq][mtype][(from_aa, to_aa)] = copy_number
+
+        # If this is the first time the sequence has been seen, add it to the
+        # unique count.
+        if seq_replaced not in self._seen:
+            record['unique'][uniq][mtype][(from_aa, to_aa)] = 1
+            self._seen.add(seq_replaced)
+
+class CloneMutations(object):
+    def __init__(self, session, clone):
         self._clone = clone
         self._session = session
-
-        if seqs is None:
-            self._seqs = session.query(Sequence).filter(
-                Sequence.clone == clone).all()
-        else:
-            self._seqs = seqs
 
         self._germline = self._clone.group.germline
         self._germline = ''.join([
@@ -46,44 +77,32 @@ class MutationCount(object):
 
     def calculate(self, commit_seqs=False):
         sample_mutations = {}
+        total_mutations = ContextualMutations()
 
-        for seq in self._seqs:
-            seq_mutations = {}
+        for seq in session.query(Sequence).filter(
+                Sequence.clone == clone):
+            seq_mutations = []
             for i in range(0, len(seq.sequence)):
                 mtype = self._get_mutation(seq.sequence, i)
-                if mtype is not None:
-                    # String for jsonifying
-                    key = '_'.join(map(str, (i, self._germline[i],
-                                             seq.sequence[i])))
-                    if key not in seq_mutations:
-                        seq_mutations[key] = {
-                            'type': mtype,
-                        }
+                if mtype is None:
+                    continue
+                seq_mutations.add((i, self._germline[i], seq.sequence[i],
+                                   from_aa, to_aa))
 
-                    if seq.sample_id not in sample_mutations:
-                        sample_mutations[seq.sample_id]['mutations'] = {
-                            'unique': {},
-                            'total': {},
-                            'mutations': {}
-                        }
-                    if key not in sample_mutations[seq.sample_id]:
-                        sample_mutations[seq.sample_id]['mutations'][key] = {
-                            'type': mtype,
-                            'unique': 0,
-                            'total': 0,
-                        }
-                if commit_seqs:
-                    seq.mutations_from_clone = json.dumps(seq_mutations)
+                mutation = (i, self._germline[i], seq.sequence[i],
+                            self._get_aa_at(self._germline, i) or '?',
+                            self._get_aa_at(seq.sequence, i) or '?')
+                if seq.sample_id not in sample_mutations:
+                    sample_mutations[seq.sample_id] = ContextualMutations()
+                sample_mutations[seq.sample_id].add_mutation(
+                        seq.sequence_replaced, mtype, mutation, seq.copy_number)
 
-            # Update the sample mutations
-            sample_mutations[seq.sample_id]['mutations'][key]['unique'] += 1
-            sample_mutations[seq.sample_id]['mutations'][key]['total'] += (
-                    seq.copy_number)
-            # Update the sample counts
-            sample_mutations[seq.sample_id]['unique'][mtype] += 1
-            sample_mutations[seq.sample_id]['total'][mtype] += seq.copy_number
+                total_mutations.add_mutation(seq.sequence_replaced, mtype,
+                                             mutation, seq.copy_number)
+            if commit_seqs:
+                seq.mutations_from_clone = json.dumps(seq_mutations)
 
         if commit_seqs:
             self._session.commit()
 
-        return sample_mutations
+        return sample_mutations, all_mutations
