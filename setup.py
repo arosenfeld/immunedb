ac68ec5642264ebebf802a9e38d0d9d1a9a42ff7
Aaron Rosenfeld <ar374@drexel.edu>
2015-05-07 12:02:13 -0400

    v0.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ dnautils = Extension('dnautils', sources=['lib/dnautils.c'])
 
 setup(
     name='SLDB',
-    version='0.7.0-1',
+    version='0.7.0',
     author='Aaron M. Rosenfeld',
     author_email='ar374@drexel.edu',
     packages=[
