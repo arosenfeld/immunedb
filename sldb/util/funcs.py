96a808afa904804ab764793afa0c7937ef9ff10e
Aaron Rosenfeld <ar374@drexel.edu>
2015-06-11 11:26:36 -0400

    removing page_query

--- sldb/util/funcs.py
+++ sldb/util/funcs.py
@@ -58,24 +58,6 @@ def get_or_create(session, model, **kwargs):
         return instance, True
 
 
-def page_query(q, per=10000, updating=False):
-    """Pages a query in case it's too large for memory.  Extreme caution needs
-    to be used if `updating` is True as rows may be returned multiple times
-    unless the updates exclude the rows from further paging"""
-    offset = 0
-    while True:
-        r = False
-        q = q.limit(per)
-        if not updating:
-            q = q.offset(offset)
-        for elem in q:
-            r = True
-            yield elem
-        offset += per
-        if not r:
-            break
-
-
 def find_streak_position(s1, s2, max_streak):
     '''Finds the first streak of max_streak characters where s1 does not equal
     s2
