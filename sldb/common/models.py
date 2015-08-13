3d9820d48a95faf5769e1671529a355a464d9af7
Aaron Rosenfeld <ar374@drexel.edu>
2015-08-13 10:05:32 -0400

    adding isotypes to samples

--- sldb/common/models.py
+++ sldb/common/models.py
@@ -108,6 +108,8 @@ class Sample(BaseMaster):
     subset = Column(String(128))
     tissue = Column(String(16))
     site = Column(String(16))
+    ig_class = Column(String(8))
+
     disease = Column(String(32))
     lab = Column(String(128))
     experimenter = Column(String(128))
