46e59220f49e46026711d16c2b9fdc01639dcc3d
Aaron Rosenfeld <ar374@drexel.edu>
2015-10-05 14:38:25 -0400

    db setup

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+echo "innodb_large_prefix = ON" >> /etc/mysql/my.cnf
+echo "innodb_file_format = Barracuda" >> /etc/mysql/my.cnf
 sldb_admin create root test_db . --admin-pass ''
