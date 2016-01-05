0a1ba6a647eaa8a91dfff2c43c769c907c62ec79
Aaron Rosenfeld <ar374@drexel.edu>
2016-01-05 12:48:07 -0500

    bumping to 0.14.1 [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dnautils = Extension('dnautils', sources=['lib/dnautils.c'],
 
 setup(
     name='SLDB',
-    version='0.14.0',
+    version='0.14.1',
     author='Aaron M. Rosenfeld',
     author_email='ar374@drexel.edu',
     url='https://github.com/arosenfeld/sldb',
