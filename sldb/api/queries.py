637650a85d2037d2b1ec74a35b72df0065864f0f
Aaron Rosenfeld <ar374@drexel.edu>
2016-01-21 11:19:00 -0500

    in-memory optimization for querying clone overlap

--- sldb/api/queries.py
+++ sldb/api/queries.py
@@ -329,27 +329,37 @@ def get_clone_overlap(session, sample_ids, filter_type, paging=None):
     """Gets a list of clones and the samples in `samples` which they appear"""
     res = []
 
-    sq_alias = aliased(CloneStats)
-    exists_clauses = [
-        CloneStats.clone_id == sq_alias.clone_id,
-        sq_alias.sample_id.in_(sample_ids),
-    ]
-    if filter_type != 'clones_all':
-        exists_clauses.append(
-            CloneStats.functional == (filter_type == 'clones_functional')
-        )
-
     clones = session.query(
         CloneStats.clone_id,
+        CloneStats.functional,
         CloneStats.unique_cnt,
         CloneStats.total_cnt
     ).filter(
-        exists().where(
-            and_(*exists_clauses)
-        ),
         CloneStats.sample_id.is_(None)
-    ).order_by(desc(CloneStats.unique_cnt))
-    clones = _page_query(clones, paging)
+    )
+
+    # NOTE: This is not part of the above query since for very large datasets
+    # doing this in-memory drastically reduces processing time
+    clones_present = set([c.cid for c in
+        session.query(distinct(CloneStats.clone_id).label('cid')).filter(
+        CloneStats.sample_id.in_(sample_ids)
+        )
+    ])
+
+    if filter_type != 'clones_all':
+        clones = [c for c in clones
+            if c.functional == (filter_type == 'clones_functional') and
+            c.clone_id in clones_present
+        ]
+    clones = sorted(
+        clones,
+        cmp=lambda a, b: cmp(b.unique_cnt, a.unique_cnt)
+    )
+
+    if paging:
+        start, per_page = paging
+        clones = clones[start:start + per_page]
+    # END NOTE
 
     for clone_id, unique_cnt, total_cnt in clones:
         selected_samples = []
