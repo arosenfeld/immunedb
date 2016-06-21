ae7e9bd21c3a0e54571858c38237e07269b14622
Aaron Rosenfeld <ar374@drexel.edu>
2016-06-21 12:03:20 -0400

    updates for docker

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export API_ENDPOINT=${API_ENDPOINT:-http://localhost:5000}
-export BASE_URL=${BASE_URL:-http://localhost:8080}
+export BASENAME=${BASENAME:-http://localhost:8080}
 export DB_VOLUME=${DB_VOLUME:-~/immunedb/db}
 export DATA_VOLUME=${DATA_VOLUME:-~/immunedb/data}
 export API_PORT=${API_PORT:-5000}
