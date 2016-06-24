d5c6abf0b9099f2fc628ae2a834b5c0ba8783a97
Aaron Rosenfeld <ar374@drexel.edu>
2016-06-24 10:02:26 -0400

    more tests

---
+++ tests/run_server.py
@@ -0,0 +1,10 @@
+from gevent import monkey
+monkey.patch_all()
+from argparse import Namespace
+
+from immunedb.api.rest_service import run_rest_service
+import immunedb.common.config as config
+
+session = config.init_db('test_db.json', as_maker=True)
+run_rest_service(session, Namespace(port=8891, debug=False,
+                                    allow_shutdown=True))
