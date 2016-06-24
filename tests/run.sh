3b90f1c38a60c16330d0b03a99ab3b1947229ebe
Aaron Rosenfeld <ar374@drexel.edu>
2016-06-24 11:08:56 -0400

    filter tests

--- tests/run.sh
+++ tests/run.sh
@@ -28,7 +28,7 @@ coverage erase
 coverage run --source=immunedb -p -m nose -s tests/tests_parser.py
 coverage run --source=immunedb -p -m nose -s tests/tests_import.py
 coverage run --source=immunedb -p -m nose -s tests/tests_pipeline.py
-coverage run --source=immunedb -p -m nose -s tests/run_server.py &
+coverage run --source=immunedb --concurrency=gevent -p -m nose -s tests/run_server.py &
 REST_PID=$!
 sleep 2
 coverage run --source=immunedb -p -m nose -s tests/tests_api.py
