7f0c84be321247a593ed1fd9347d063be09376da
Aaron Rosenfeld <ar374@drexel.edu>
2016-07-19 11:52:38 -0400

    import work

--- immunedb/importing/clones.py
+++ immunedb/importing/clones.py
@@ -1,6 +1,7 @@
 import csv
 
 from immunedb.common.models import Sequence, SequenceCollapse, Subject
+from immunedb.aggregates.clones import generate_consensus
 from immunedb.importing import ImportException
 
 def generate_template(session, out_file):
@@ -8,6 +9,7 @@ def generate_template(session, out_file):
         writer = csv.DictWriter(fh, delimiter='\t', fieldnames=[
             'ai',
             'seq_id',
+            'subject_id',
             'subject',
             'sequence',
             'v_gene',
@@ -20,10 +22,13 @@ def generate_template(session, out_file):
         ])
 
         writer.writeheader()
-        for seq in session.query(Sequence).join(SequenceCollapse).join(Subject):
+        template_seqs = session.query(
+            Sequence).join(SequenceCollapse).join(Subject)
+        for seq in template_seqs:
             writer.writerow({
                 'ai': seq.ai,
                 'seq_id': seq.seq_id,
+                'subject_id': seq.subject.id,
                 'subject': seq.subject.identifier,
                 'sequence': seq.sequence,
                 'v_gene': seq.v_gene,
@@ -47,19 +52,33 @@ def import_template(session, in_file):
         for line in reader:
             if line['clone_id'] in ('None', '0', ''):
                 continue
-            clone = seen_clones.setdefault(line['clone_id'], {
-                'v_gene': line['v_gene'],
-                'j_gene': line['j_gene'],
-                'cdr3_num_nts': len(line['cdr3_nt']),
-                'ais': []
+            clone_info = seen_clones.setdefault(line['clone_id'], {
+                'clone': Clone(subject_id=int(line['subject_id']),
+                               v_gene: line['v_gene'],
+                               j_gene: line['j_gene'],
+                               cdr3_num_nts: len(line['cdr3_nt'])),
+                'seqs': []
             })
-            print clone
-            if (clone['v_gene'] != line['v_gene'] or
-                    clone['j_gene'] != line['j_gene'] or
-                    clone['cdr3_num_nts'] != len(line['cdr3_nt'])):
-                raise ImportException('Sequence with ai {} was assigned to '
-                                      'clone {} with mismatching v_gene, '
-                                      'j_gene, or cdr3_num_nts'.format(
-                                          line['ai'], line['clone_id']
-                                      ))
-            clone['ais'].append(line['ai'])
+            clone_inst = clone_info['clone']
+            if (clone_inst['v_gene'] != line['v_gene'] or
+                    clone_inst['j_gene'] != line['j_gene'] or
+                    clone_inst['cdr3_num_nts'] != len(line['cdr3_nt']),
+                    clone_inst['subject_id'] == int(line['subject_id'])):
+                raise ImportException(
+                    'Sequence with ai {} was assigned to clone {} with '
+                    'mismatching v_gene, j_gene, cdr3_num_nts, or '
+                    'subject.'.format(line['ai'], line['clone_id']))
+            clone_info['seqs'].append({
+                'ai': line['ai'],
+                'cdr3_nt': line['cdr3_nt']
+            })
+
+
+def create_clones(session, clone_mapping):
+    db_clone_ids = set([])
+    for clone_id, clone_info in clone_mapping.iteritems():
+        clone_inst = clone_info['clone']
+        session.add(clone_inst)
+        session.flush()
+        db_clone_ids.add(clone_inst.id)
+    generate_consensus(session, db_clone_ids)
