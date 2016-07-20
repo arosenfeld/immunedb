fd1dd097aea11454a6af0566cb843c3c85c3566e
Aaron Rosenfeld <ar374@drexel.edu>
2016-07-20 15:54:18 -0400

    clone import and associated tests

---
+++ tests/tests_clone_import.py
@@ -0,0 +1,36 @@
+from regression import NamespaceMimic, BaseTest
+
+import immunedb.common.config as config
+from immunedb.common.models import Clone, Sequence
+from immunedb.importing.clones import generate_template, import_template
+
+from regression import CONFIG_PATH
+
+
+class TestCloneImport(BaseTest.BaseRegression):
+    def __init__(self, *args, **kwargs):
+        super(TestCloneImport, self).__init__('clone_import', *args, **kwargs)
+
+    def setUp(self):
+        self.session = config.init_db(CONFIG_PATH)
+
+    def test_import(self):
+        import_template(
+            self.session,
+            'tests/data/clone_import/assigned.tsv',
+            True
+        )
+
+        self.regression(
+            self.get_path('post_clone_import_clones.json'),
+            self.session.query(Clone),
+            'id',
+            ('id', 'functional', 'v_gene', 'j_gene', 'insertions',
+             'cdr3_nt', 'cdr3_num_nts', 'cdr3_aa', 'germline'),
+        )
+        self.regression(
+            self.get_path('post_clone_import_assignment.json'),
+            self.session.query(Sequence),
+            'seq_id',
+            ('seq_id', 'clone_id'),
+        )
