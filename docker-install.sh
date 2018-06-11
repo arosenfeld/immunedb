e7f47bb4ca7b3a4faa6d48ecb99bb20464dec072
Aaron Rosenfeld <ar374@drexel.edu>
2018-06-11 10:19:35 -0400

    removing unused scripts and updating changelog

--- docker-install.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/arosenfeld/immunedb/master/immunedb-docker.sh
-wget https://raw.githubusercontent.com/arosenfeld/immunedb/master/docker-compose.yml
-chmod +x immunedb-docker.sh
