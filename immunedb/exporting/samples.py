57e9c1e886136bec48077a61bc7995fd3d8130ee
Aaron Rosenfeld <ar374@drexel.edu>
2018-08-10 14:26:55 -0400

    clone counts for sample exporting

--- immunedb/exporting/samples.py
+++ immunedb/exporting/samples.py
@@ -1,4 +1,6 @@
-from immunedb.common.models import Sample, SampleMetadata
+from sqlalchemy import func
+
+from immunedb.common.models import CloneStats, Sample, SampleMetadata
 from immunedb.exporting.tsv_writer import StreamingTSV, write_tsv
 from immunedb.util.log import logger
 
@@ -8,8 +10,17 @@ def get_samples(session):
         s.key for s in session.query(SampleMetadata.key).group_by(
             SampleMetadata.key).order_by(SampleMetadata.key)
     ]
+
+    clone_cnts = {s.sample_id: s.clones for s in session.query(
+        CloneStats.sample_id,
+        func.count(CloneStats.clone_id.distinct()).label('clones')
+    ).filter(
+        ~CloneStats.sample_id.is_(None)
+    ).group_by(CloneStats.sample_id)}
+
     writer = StreamingTSV(['id', 'name', 'subject', 'input_sequences',
-                           'identified', 'in_frame', 'stops', 'functional'] +
+                           'identified', 'in_frame', 'stops', 'functional',
+                           'clones'] +
                           meta)
     yield writer.writeheader()
     for sample in session.query(Sample).order_by(Sample.name):
@@ -22,7 +33,8 @@ def get_samples(session):
             sample.stats.no_result_cnt,
             'in_frame': sample.stats.in_frame_cnt,
             'stops': sample.stats.stop_cnt,
-            'functional': sample.stats.functional_cnt
+            'functional': sample.stats.functional_cnt,
+            'clones': clone_cnts[sample.id]
         }
         row.update(sample.metadata_dict)
         yield writer.writerow(row)
