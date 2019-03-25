c07c3d3e6a0fd8623e67d0685fecca20037fe18a
Aaron Rosenfeld <aaron.rosenfeld@pennmedicine.upenn.edu>
2019-03-25 10:03:05 -0400

    bumping to v0.28.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='ImmuneDB',
-    version='0.28.1',
+    version='0.28.2',
     author='Aaron M. Rosenfeld',
     author_email='ar374@drexel.edu',
     url='https://github.com/arosenfeld/immunedb',
